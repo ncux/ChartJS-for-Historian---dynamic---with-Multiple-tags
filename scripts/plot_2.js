b5eeec1340bcf97f0de3397b4b816e0a45a59d83
Talent Malaba <malabatalent@gmail.com>
2018-12-24 09:11:44 +0800

    1st commit

---
+++ scripts/plot_2.js
@@ -0,0 +1,192 @@
+
+let API = {
+    access_token: "",
+    tagsUrl: 'https://dev.sealu.net:4433/api/v1/forward?url=/historian-rest-api/v1/tagslist',
+    dataUrl: "https://dev.sealu.net:4433/api/v1/forward?url=/historian-rest-api/v1/datapoints/calculated"
+};
+
+
+// user inputs
+const form = document.querySelector('#form');
+const tagSelector = document.querySelector('#tagSelector');
+// const tagSelector2 = document.querySelector('#tagSelector2');
+// const tagSelector3 = document.querySelector('#tagSelector3');
+// const tagSelector4 = document.querySelector('#tagSelector4');
+const startDate = document.querySelector('#startDate');
+const endDate = document.querySelector('#endDate');
+const startTime = document.querySelector('#startTime');
+const endTime = document.querySelector('#endTime');
+const count = document.querySelector('#count');
+const interval = document.querySelector('#interval');
+const plotType = document.querySelector('#plotType');
+const plotButton = document.querySelector('#plotButton');
+const warning = document.querySelector('#warning');
+
+let tagSelectorsArray = document.querySelectorAll('.tagSelectors');
+
+const table = document.querySelector('#table');
+const tableCaption = document.querySelector('#tableCaption');
+
+// grabs the canvas
+const ctx = document.querySelector('#chart').getContext('2d');
+
+// holds the API tags
+let tagsArray = [];
+
+
+// these two arrays hold the chart values to be plotted
+let valuesArray = [];
+let timeArray = [];
+
+
+// chart parameters
+let data = {
+    labels: timeArray,
+    datasets: [{
+        label: tagSelector.value,
+        data: valuesArray,
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+};
+
+
+// chart options
+let options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:false
+            }
+        }]
+    }
+};
+
+
+// gets tags
+async function getTags() {
+
+    // const options = { headers: { 'Authorization': `Bearer ${ API.access_token }` } };
+    // let response = await fetch(API.tagsUrl, options);
+
+    let response = await fetch(`./data/tags - verbose.json`);
+
+    let data = await response.json();
+    let tags = data.Tags;
+    // console.log(tags);
+    tags.map(tag => {
+        let allTags = tag.Tagname;
+        tagsArray.push(allTags);
+        populateTagsInput();
+    });
+}
+
+
+// populates the tags dropdown menu
+function populateTagsInput() {
+
+    tagsArray.forEach(tag => {
+
+        tagSelectorsArray.forEach(selector => {
+            let tagOption = document.createElement('option');
+            tagOption.textContent = tag;
+            tagOption.setAttribute('value', tag);
+            selector.append(tagOption);
+        });
+    });
+}
+
+
+plotButton.addEventListener('click', checkIfFormIsFullyFilled);
+
+
+function checkIfFormIsFullyFilled(e) {
+    e.preventDefault();
+    console.log('button clicked');
+
+    let formInputs = form.elements;
+    let emptyFields = [...formInputs].some(input => input.value === '');  // boolean
+    if (emptyFields) {
+        warning.style.display = 'block';
+    } else {
+        getValuesThenPlotChartAndTabulateData();
+    }
+}
+
+
+async function getValuesThenPlotChartAndTabulateData() {
+
+    let queryUrl = generateQueryUrl();
+    console.log(queryUrl);
+
+    const options = { headers: { 'Authorization': `Bearer ${ API.access_token }` } };
+    let response = await fetch(queryUrl, options);
+
+    let historianData = await response.json();
+    let timeStampsAndValues = historianData['Data'][0].Samples;
+    console.log(timeStampsAndValues);
+
+    // fill the chart arrays & plot the chart
+    timeStampsAndValues.forEach(value => {
+        timeArray.push(simplifyTime(value.TimeStamp));
+        valuesArray.push((parseInt(value.Value)).toFixed(0));   // removing decimal fraction
+        plotChart();
+    });
+
+    // tabulate the data
+    tableCaption.textContent = `Data from tag ${tagSelector.value}`;
+    timeStampsAndValues.forEach(dataItem => {
+        let row = document.createElement('tr');
+        row.innerHTML = `<td>${simplifyTime(dataItem.TimeStamp)}</td> <td>${(parseInt(dataItem.Value)).toFixed(0)}</td> <td>${dataItem.Quality}</td>`;
+        table.append(row);
+    });
+
+}
+
+
+// grabs the form inputs and builds the API query URL
+function generateQueryUrl() {
+    // change interval value to milliseconds
+    const milliseconds = Math.ceil((parseInt(interval.value))*1000);
+    return `${API.dataUrl}/${tagSelector.value}/${startDate.value}T${startTime.value}/${endDate.value}T${endTime.value}/1/${count.value}/${milliseconds}`;
+}
+
+
+// trims off the seconds
+function simplifyTime(timestamp) {
+    return timestamp.slice(0, 16);
+}
+
+
+function plotChart() {
+    const chart = new Chart(ctx, {
+        type: plotType.value,
+        data: data,
+        options: options
+    });
+}
+
+
+
+
+
+
+
+
+
+
